a5d418570bd0dbc200b4adb05e87843e154fa6d6
Brian Harris <sendbrianemail@gmail.com>
2017-02-28 14:59:54 -0500

    updated ccda validator endpoint to use TTP.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   r1_url: 'https://prodccda.sitenv.org/CCDAValidatorServices/r1.1/',
-  r2_url: 'http://ttpds.sitenv.org:8080/referenceccdaservice/',
+  r2_url: 'https://ttpds.sitenv.org:8443/referenceccdaservice/',
   trustanchor_upload_url: 'https://prodccda.sitenv.org/trustanchoruploader/uploadtrustanchor',
   trustbundle_download_url: 'https://prodccda.sitenv.org/trustanchoruploader/downloadtrustbundle',
   direct_send_precanned_message_url: 'https://prodccda.sitenv.org/directtransportmessagesender/sendmessagewithattachmentfilepath',
