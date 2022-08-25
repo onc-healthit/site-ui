f67d5d40ce5096c06bb4e27363a0f3a564b1c7b4
Dan Brown <dbrown.se@gmail.com>
2022-08-25 15:23:22 -0400

    Add SVAP to routing and module

--- src/app/+ccda-validator-module/ccda-validator.module.ts
+++ src/app/+ccda-validator-module/ccda-validator.module.ts
@@ -1,19 +1,20 @@
-import {NgModule} from "@angular/core";
-import {HttpModule} from "@angular/http";
-import {Ng2Bs3ModalModule} from "ng2-bs3-modal/ng2-bs3-modal";
-import {CCDAValidatorService} from "../shared/ccda-validator.service";
-import {CCDAValidatorRoutingModule} from "./ccda-validator.routing";
-import {XmlHighlightComponent} from "./xml-highlight/xml-highlight.component";
-import {ResultsListComponent} from "./results-list/results-list.component";
-import {HighlightCcdaXmlResultDirective} from "./xml-highlight/highlight-ccda-xml-result.directive";
-import {SharedModule} from "../shared/shared.module";
-import {CcdaR1ValidatorComponent} from "./ccda-r1-validator-component/ccda-r1-validator.component";
-import {CcdaValidatorReleaseNotesComponent} from "./ccda-validator-release-notes-component/ccda-validator-release-notes.component";
-import {CcdaR2ValidatorComponent} from "./ccda-r2-validator-component/ccda-r2-validator.component";
-import {CcdaR2CuresValidatorComponent} from "./ccda-r2-cures-validator-component/ccda-r2-cures-validator.component";
-import {CcdaValidatorLauncherComponent} from "./ccda-validator-launcher/ccda-validator-launcher.component";
-import {AccordionModule} from "ngx-accordion";
-import {Angulartics2Module} from "angulartics2";
+import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
+import { Ng2Bs3ModalModule } from "ng2-bs3-modal/ng2-bs3-modal";
+import { CCDAValidatorService } from "../shared/ccda-validator.service";
+import { CCDAValidatorRoutingModule } from "./ccda-validator.routing";
+import { XmlHighlightComponent } from "./xml-highlight/xml-highlight.component";
+import { ResultsListComponent } from "./results-list/results-list.component";
+import { HighlightCcdaXmlResultDirective } from "./xml-highlight/highlight-ccda-xml-result.directive";
+import { SharedModule } from "../shared/shared.module";
+import { CcdaR1ValidatorComponent } from "./ccda-r1-validator-component/ccda-r1-validator.component";
+import { CcdaValidatorReleaseNotesComponent } from "./ccda-validator-release-notes-component/ccda-validator-release-notes.component";
+import { CcdaR2ValidatorComponent } from "./ccda-r2-validator-component/ccda-r2-validator.component";
+import { CcdaR2CuresValidatorComponent } from "./ccda-r2-cures-validator-component/ccda-r2-cures-validator.component";
+import { CcdaR2Svap2022ValidatorComponent } from "./ccda-r2-svap2022-validator-component/ccda-r2-svap2022-validator.component";
+import { CcdaValidatorLauncherComponent } from "./ccda-validator-launcher/ccda-validator-launcher.component";
+import { AccordionModule } from "ngx-accordion";
+import { Angulartics2Module } from "angulartics2";
 
 @NgModule({
   imports: [
@@ -36,9 +37,9 @@ import {Angulartics2Module} from "angulartics2";
     CcdaValidatorLauncherComponent,
     CcdaValidatorReleaseNotesComponent
   ],
-  providers:[
+  providers: [
     CCDAValidatorService
   ],
 })
 
-export class CCDAValidatorModule{}
+export class CCDAValidatorModule { }
--- src/app/+ccda-validator-module/ccda-validator.routing.ts
+++ src/app/+ccda-validator-module/ccda-validator.routing.ts
@@ -1,15 +1,20 @@
 /**
  * Created by Brian on 10/17/2016.
  */
-import {NgModule} from "@angular/core";
-import {Routes, RouterModule} from "@angular/router";
-import {CcdaR1ValidatorComponent} from "./ccda-r1-validator-component/ccda-r1-validator.component";
-import {CcdaR2ValidatorComponent} from "./ccda-r2-validator-component/ccda-r2-validator.component";
-import {CcdaR2CuresValidatorComponent} from "./ccda-r2-cures-validator-component/ccda-r2-cures-validator.component";
-import {CcdaValidatorLauncherComponent} from "./ccda-validator-launcher/ccda-validator-launcher.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CcdaR1ValidatorComponent } from "./ccda-r1-validator-component/ccda-r1-validator.component";
+import { CcdaR2ValidatorComponent } from "./ccda-r2-validator-component/ccda-r2-validator.component";
+import { CcdaR2CuresValidatorComponent } from "./ccda-r2-cures-validator-component/ccda-r2-cures-validator.component";
+import { CcdaR2Svap2022ValidatorComponent } from "./ccda-r2-svap2022-validator-component/ccda-r2-svap2022-validator.component";
+import { CcdaValidatorLauncherComponent } from "./ccda-validator-launcher/ccda-validator-launcher.component";
 
 
 const routes: Routes = [
+  {
+    path: 'ccdar2svap2022validator',
+    component: CcdaR2Svap2022ValidatorComponent
+  },
   {
     path: 'ccdar2curesvalidator',
     component: CcdaR2CuresValidatorComponent
