0aa26f566aa6aca619c90a2c6951db456c90b8a0
Girish <bgirishrao@drajer.com>
2023-09-27 20:44:27 -0400

    Added node express

--- server/server.js
+++ server/server.js
@@ -15,10 +15,6 @@ const api = require('./server/routes/api');
 const app = express();
 
 app.use(express);
-// Parsers for POST data
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser());
 
 app.use(bodyParser.json());
 app.use(forms.array()); 
@@ -27,16 +23,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload({
     createParentPath: true
 }));
-
-  // app.use(upload.array()); 
-  // app.use(express.static('public'));
-
-// app.use( bodyParser.json({limit: '50mb'}) );
-// app.use(bodyParser.urlencoded({
-//   limit: '50mb',
-//   extended: true,
-//   parameterLimit:50000
-// }));
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -52,7 +38,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '80';
+const port = process.env.SITE_PORT || '80';
 app.set('port', port);
 
 /**
--- src/app/shared/ccda-validator.service.ts
+++ src/app/shared/ccda-validator.service.ts
@@ -5,7 +5,6 @@ import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import { Observable } from 'rxjs/Rx';
 import { HttpClient } from  '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
 import { environment } from "../../environments/environment";
 
 const keyCloakURL = environment.jwt_accesstoken_endpoint;
