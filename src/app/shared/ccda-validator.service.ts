8589e0abf229bb4cd0dfa91aa86b9dee6299f425
Girish <bgirishrao@drajer.com>
2023-12-18 12:03:15 -0500

    reverted node

--- src/app/shared/ccda-validator.service.ts
+++ src/app/shared/ccda-validator.service.ts
@@ -19,7 +19,7 @@ constructor(private  http : HttpClient){}
     severityLevelSelected: string, curesUpdate: boolean = false, svap2022: boolean = false,svap2023: boolean = false): any {
     return new Promise((resolve, reject) => {
       let formData: any = new FormData();
-      // let xhr = new XMLHttpRequest();
+      let xhr = new XMLHttpRequest(); // comment this for node to run
       for (let i = 0; i < files.length; i++) {
         console.log("form data append ccdaFile ....."+files[i].name);
         formData.append("ccdaFile", files[i], files[i].name);
@@ -31,19 +31,36 @@ constructor(private  http : HttpClient){}
       formData.append("svap2022", svap2022);
       formData.append("svap2023", svap2023);
       formData.append("severityLevel", severityLevelSelected);
-      formData.append("ccdaurl", url);
 
-      formData.append("keycloakurl", keyCloakURL);
-      formData.append("clientid", keyClientID);
 
-      this.http.post("/api",formData).subscribe(response => {
-        console.log(" response ::::"+response);
-        resolve(response);
-      },(error ) =>{
-        console.log("with httpOptions JSON.stringify ::::::"+JSON.stringify(error));
-        reject("ERROR_CCDA");
+      // uncomment this for node to run
+      // formData.append("ccdaurl", url);
+
+      // formData.append("keycloakurl", keyCloakURL);
+      // formData.append("clientid", keyClientID);
+
+      // this.http.post("/api",formData).subscribe(response => {
+      //   console.log(" response ::::"+response);
+      //   resolve(response);
+      // },(error ) =>{
+      //   console.log("with httpOptions JSON.stringify ::::::"+JSON.stringify(error));
+      //   reject("ERROR_CCDA");
+      // }
+      // );
+
+// comment this for node to run
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(xhr.response);
+          }
+        }
       }
-      );
+      xhr.open("POST", url, true);
+      xhr.send(formData);
+      // till here 
     })
   }
 
